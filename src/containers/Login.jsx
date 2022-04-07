3461cbcb795999c59324d4d65eca63b8344f696c
Kevin Alberto Palma Ralda <kapalma05@gmail.com>
2022-04-06 23:35:20 -0600

    :construction: Work in login with react and sass
    Work in login with react and sass

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -11,18 +11,24 @@ const Login = () => {
                 
                 <form action className="form">
                     <div className="input-group">
-                        <input className="input input-email" type="text" id="email" placeholder="Email" autoComplete="off" required />
-                    </div>
+                        <input className="input input-email" type="email" id="email" placeholder="Email" autoComplete="off" required />
+                        <i class="uil uil-at"></i>                    </div>
                     <div className="input-group">
                         <input className="input input-password" type="password" id="password" placeholder="Password" autoComplete="off" required />
-                    </div>
-                    <button type="submit" className="primary-btn password-btn">
-                        <span className="text-btn">Login</span>
+                        <i class="uil uil-key-skeleton"></i>                    </div>
+                    <button type="submit" className="primary-btn login-btn">
+                        <span className="text-btn">
+                            <i class="login-icon uil uil-signin"></i>
+                            Login
+                        </span>
                     </button>
-                    <a href className="forgot">Forgot my password</a>
+                    <a href="#" className="forgot">Forgot my password</a>
                 </form>
-                <a href className="secondary-btn signup-btn">
-                    <span>Signup</span>
+                <a href="#" className="secondary-btn signup-btn">
+                    <span className='text-btn'>
+                        <i class="uil uil-user-check"></i>
+                        Signup
+                    </span>
                 </a>
             </div>
         </div>
